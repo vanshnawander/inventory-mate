5b800f1009067f9393e6af7b71df462fef4377b0
vansh <vanshnawander@gmail.com>
2023-06-13 19:25:20 +0530

    url pattern updated

--- inventory/urls.py
+++ inventory/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='/home'),
+    path('login/',views.login, name='login'),
+    path('raiseorder/',views.ProductOrder, name='productorder'),
+    path('checkstock/',views.inventory_stock, name='inventory stock'),
+    path('modifyorder/',views.OrderAuth, name='modify order'),
+    path('sendordertovendor/',views.OrderFinalise, name='sending order to vendor'),
+    path('home_manager/',views.Home_manager, name='home page for manager'),
+    path('home_admin/',views.Home_admin, name='home page admin'),
+    
 ]
