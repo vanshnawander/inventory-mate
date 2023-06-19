353b4ac0b8267cea782500ca427c2fb417eed05c
krunal <krunalsam1@gmail.com>
2023-06-20 00:35:05 +0530

    ff

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class inventory(models.Model):
+class Product_details(models.Model):
     serial_number = models.IntegerField()
     product_name = models.CharField(max_length=50)
     UOM = models.CharField(max_length=10)
