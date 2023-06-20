351591b5080bb9caaef7fc7b95bf59f94b21ae37
Keerthan-Reddy <keerthan.9492@gmail.com>
2023-06-20 19:31:13 +0530

    models.py updated

--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class inventory(models.Model):
+class Product_details(models.Model):
     serial_number = models.IntegerField()
     product_name = models.CharField(max_length=50)
     UOM = models.CharField(max_length=10)
@@ -11,3 +11,33 @@ class inventory(models.Model):
     price = models.FloatField()
     primary_vendor = models.CharField(max_length=30)
     quantity_available = models.FloatField()
+    
+class list_of_po_iteams(models.Model):
+    po_id=models.IntegerField();
+    product_name=models.CharField(max_length=50)
+    product_price=models.FloatField()
+    vendor_name=models.CharField(max_length=30)
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    date=models.DateField(auto_now_add=True)
+    quantity=models.IntegerField()
+    status=models.CharField(max_length=30)
+    user_name=models.CharField(max_length=30)
+    
+class vendor_details(models.Model):
+    vendor_id=models.IntegerField()
+    vendor_name=models.CharField(max_length=30)
+    vendor_phone=models.IntegerField()
+
+class vendor_price(models.Model):
+    vendor_id=models.IntegerField()
+    product_name=models.CharField(max_length=50)
+    product_price=models.FloatField()
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    
+class inventory_in_hand(models.Model):
+    product_name=models.CharField(max_length=50)
+    quantity=models.IntegerField()
+    time_stamp=models.DateTimeField(auto_now_add=True)
+    
+
+   
