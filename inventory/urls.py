58e8e000c546c420839f1e5d8f6a035cb67dcc60
vanshnawander <vanshnawander@gmail.com>
2023-07-03 21:59:57 +0530

    send

--- inventory/urls.py
+++ inventory/urls.py
@@ -18,15 +18,18 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('<path:unknown_url>/', views.no_such_url_view, name='no_such_url'),
+    # path('<path:unknown_url>/', views.no_such_url_view, name='no_such_url'),
     path('admin/', admin.site.urls),
-    path('', views.home, name='/home'),
-    path('login/',views.login, name='login'),
-    path('raiseorder/',views.ProductOrder, name='productorder'),
-    path('checkstock/',views.inventory_stock, name='inventory stock'),
-    path('modifyorder/',views.OrderAuth, name='modify order'),
-    path('sendordertovendor/',views.OrderFinalise, name='sending order to vendor'),
-    path('home_manager/',views.Home_manager, name='home page for manager'),
-    path('home_admin/',views.Home_admin, name='home page admin'),
-    
+    path('', views.SMhome, name='SMhome'),
+    path('login/', views.login, name='login'),
+    path('SMhome/', views.SMhome, name='SMhome'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('createPO/', views.createPO, name='create PO'),
+    path('recieverPO/', views.recievePO, name='recievePO.html'),
+    path('issues/', views.issues, name='issues'),
+    path('sendordertovendor/', views.OrderFinalise,
+         name='sending order to vendor'),
+    path('home_admin/', views.Home_admin, name='home page admin'),
+    path('Addproducts/', views.add_products_in_inventory,
+         name='add products in inventory'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,33 +1,76 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from myapp.models import Product_details
+
 
 def no_such_url_view(request, unknown_url):
     return render(request, '404.html', {'unknown_url': unknown_url})
 
-def home(request):
-    return render(request, 'home.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html',)
+
+
+def SMhome(request):
+    if request.method == 'POST':
+        product_name = request.POST['product_name']
+        UOM = request.POST['uom']
+        tax_percentage = request.POST['tax_percentage']
+        price = request.POST['price']
+        primary_vendor = request.POST['primary_vendor']
+        quantity_available = request.POST['quantity_available']
+        table_entry = Product_details(serial_number=1, product_name=product_name, UOM=UOM, tax_percentage=tax_percentage,
+                                      price=price, primary_vendor=primary_vendor, quantity_available=quantity_available)
+        table_entry.save()
+    a = Product_details.objects.all()
+    print(a)
+    return render(request, 'SMhome.html', {'Product_details': a})
+
+
+def add_products_in_inventory(request):
+    return render(request, 'additems.html')
+
+
+def createPO(request):
+    return render(request, 'createPO.html')
+
+
+def recievePO(request):
+    return render(request, 'recievePO.html')
+
+
+def issues(request):
+    return render(request, 'issues.html')
+
 
 def ProductOrder(request):
     return render(request, 'order.html')
 
-def inventory_stock(request):
-    return render(request, 'stock.html')
 
-def OrderAuth(request):
-    return render(request, 'orderauth.html')
+def inventory(request):
+    if request.method == 'POST':
+        serial_number = request.POST['serial_number']
+        product_name = request.POST['product_name']
+        UOM = request.POST['uom']
+        tax_percentage = request.POST['tax_percentage']
+        price = request.POST['price']
+        primary_vendor = request.POST['primary_vendor']
+        quantity_available = request.POST['quantity_available']
+        par_value = request.POST['par_value']
+        table_entry = Product_details(serial_number=1, product_name=product_name, UOM=UOM, tax_percentage=tax_percentage,
+                                      price=price, primary_vendor=primary_vendor, quantity_available=quantity_available,
+                                      par_value=par_value)
+        table_entry.save()
+    a = Product_details.objects.all()
+    print(a)
+    return render(request, 'inventory.html', {'Product_details': a})
+
 
 def OrderFinalise(request):
     return render(request, 'orderFinal.html')
 
-def Home_manager(request):
-    return render(request, 'home_manager.html')
 
 def Home_admin(request):
     return render(request, 'home_admin.html')
-
-
